e5041a9af2350544f74ebf4b2f9971f76baa9676
Deepak Singh Dhami <dexter34925@yahoo.in>
2017-01-02 11:11:25 +0530

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -18,6 +18,10 @@ Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 Install-Module -Name Pester -Scope CurrentUser -Force -Verbose
 
 Describe 'testing PSSession at the one time initialization block' {
+    Write-Host "$(whoami.exe)"
+    Write-Host "Local administrators"
+    net localgroup administrators
+    
     Write-host 'Trying to create PSSession'
     $Session = New-PSSession -ComputerName localhost
 
