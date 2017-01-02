dc022da3ca093c7262b1e0f9ec17b0f7528e4a36
Deepak Singh Dhami <dexter34925@yahoo.in>
2017-01-02 12:42:24 +0530

    Switch EnableNetworkAccess  added

--- test1.ps1
+++ test1.ps1
@@ -4,11 +4,11 @@ function Test-IsAdmin {
 
 }
 
-Write-host 'Trying to create PSSession'
-$Session = New-PSSession -ComputerName localhost
+
 
 if (Test-IsAdmin) {
-
+  Write-host 'Trying to create PSSession'
+  $Session = New-PSSession -ComputerName localhost -EnableNetworkAccess
   Describe 'testing PSSession at the one time initialization block' {
 
       It 'Should open a PSSession' {
